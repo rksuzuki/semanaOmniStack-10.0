fbc404aa6cca4b026dfdc3d4a9f1a4065635c230
Rodrigo Suzuki <ro20120050@wipro.com>
2020-03-23 21:55:26 -0300

    semana omnistack 10.0 finalizado

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-x4bdu.mongodb.net/semana10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.Promise = global.Promise;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
